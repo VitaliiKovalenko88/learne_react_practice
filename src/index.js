c031f6222a0bd714dde1db93b606e9a26c3bad18
VitaliiKovalenko88 <vintilyator88@gmail.com>
2023-12-12 21:35:27 +0100

    create new bronsh

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } fro m "react-router-dom";
 import './index.css'
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename=''>
-      <App />
-    </BrowserRouter>
+    {/* <BrowserRouter basename=''> */}
+    <App />
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
 
