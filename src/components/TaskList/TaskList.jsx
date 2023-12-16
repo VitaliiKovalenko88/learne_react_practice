955076a8e4906cfed77afad3f059d983e78fc042
VitaliiKovalenko88 <75671142+VitaliiKovalenko88@users.noreply.github.com>
2023-12-16 15:52:35 +0100

    add learn-react in bransh

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,35 @@
+import { Task } from "../Task/Task";
+import css from "./TaskList.module.css";
+import { useSelector } from "react-redux";
+import { statusFilters } from "../../redux/constants";
+
+const { active, completed } = statusFilters;
+
+const getVisibleTasks = (tasks, statusFilters) => {
+  switch (statusFilters) {
+    case active:
+      return tasks.filter(({ completed }) => !completed);
+
+    case completed:
+      return tasks.filter(({ completed }) => completed);
+
+    default:
+      return tasks;
+  }
+};
+
+export const TaskList = () => {
+  const tasks = useSelector((state) => state.tasks);
+  const statusFilter = useSelector((state) => state.tasks);
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+
+  return (
+    <ul className={css.list}>
+      {visibleTasks.map((task) => (
+        <li className={css.listItem} key={task.id}>
+          <Task task={task} />
+        </li>
+      ))}
+    </ul>
+  );
+};
