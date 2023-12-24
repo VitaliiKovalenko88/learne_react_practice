e2ee01e9a9fea853517233be47b80fc6d57ed4b8
VitaliiKovalenko88 <75671142+VitaliiKovalenko88@users.noreply.github.com>
2023-12-25 00:04:42 +0100

    fixed problem AppBar pack

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -5,8 +5,8 @@ axios.defaults.baseURL = "https://62584f320c918296a49543e7.mockapi.io";
 
 export const fetchTasks = createAsyncThunk("tasks/fetchAll", async (_, thunkAPI) => {
   try {
-    const respons = await axios.get("/tasks");
-    return respons.data;
+    const response = await axios.get("/tasks");
+    return response.data;
   } catch (e) {
     return thunkAPI.rejectWithValue(e.message);
   }
@@ -15,8 +15,8 @@ export const fetchTasks = createAsyncThunk("tasks/fetchAll", async (_, thunkAPI)
 export const addTask = createAsyncThunk("tasks/addTask",
   async (text, thunkAPI) => {
     try {
-      const respons = await axios.post("/tasks", { text });
-      return respons.data;
+      const response = await axios.post("/tasks", { text });
+      return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message)
 
